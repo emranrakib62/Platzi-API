d7b1035bdd7e96fc4a6524f0afbbeceab06defb6
Emranul Hoque <emranrakib01862@gmail.com>
2025-07-24 23:03:10 +0600

    updated

--- app/src/main/java/com/example/platziapi/repositories/ProductRepositories.kt
+++ app/src/main/java/com/example/platziapi/repositories/ProductRepositories.kt
@@ -1,4 +1,11 @@
 package com.example.platziapi.repositories
 
-class ProductRepositories {
+import com.example.platziapi.network.Platziervice
+import javax.inject.Inject
+
+class ProductRepositories @Inject constructor(val platziService:Platziervice) {
+   suspend fun getAllProductRepo(){
+        platziService.getAllProduct()
+    }
+
 }
