81436c578df502c22587a58ed055bab8ad9b902c
Emranul Hoque <emranrakib01862@gmail.com>
2025-07-24 11:48:01 +0600

    updated

---
+++ app/src/main/java/com/example/platziapi/PlatziApp.kt
@@ -0,0 +1,4 @@
+package com.example.platziapi
+
+class PlatziApp {
+}
--- app/src/main/java/com/example/platziapi/di/Networkmodule.kt
+++ app/src/main/java/com/example/platziapi/di/Networkmodule.kt
@@ -1,14 +1,26 @@
 package com.example.platziapi.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+import kotlin.text.Typography.dagger
 
 @InstallIn(SingletonComponent::class)
 @Module
 class Networkmodule {
+@Provides
+@Singleton
+    fun provideRetrofit(): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
-
+    }
 
 
 }
