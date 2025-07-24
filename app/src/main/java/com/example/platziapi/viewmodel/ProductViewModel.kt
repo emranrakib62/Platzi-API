0dad6e8214ac54032a7302326e135bcb659f4718
Emranul Hoque <emranrakib01862@gmail.com>
2025-07-24 23:22:51 +0600

    updated

--- app/src/main/java/com/example/platziapi/viewmodel/ProductViewModel.kt
+++ app/src/main/java/com/example/platziapi/viewmodel/ProductViewModel.kt
@@ -3,13 +3,31 @@ package com.example.platziapi.viewmodel
 import androidx.compose.ui.geometry.MutableRect
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.platziapi.data.models.product.ResponseProductItem
 import com.example.platziapi.repositories.ProductRepositories
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltAndroidApp
-class ProductViewModel @Inject constructor(val repositories: ProductRepositories) {
+class ProductViewModel @Inject constructor(val repositories: ProductRepositories) :ViewModel(){
     private val _allproducts:MutableLiveData<ResponseProductItem> = MutableLiveData()
     val  allproducts:LiveData<ResponseProductItem> = _allproducts
+
+
+    fun getAllproductvm(){
+
+        viewModelScope.launch {
+
+            val response =repositories.getAllProductRepo()
+            if(response.isSuccessful){
+                _allproducts.postValue(response.body())
+            }
+
+        }
+
+    }
+
 }
